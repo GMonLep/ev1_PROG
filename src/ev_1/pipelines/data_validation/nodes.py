abefd64fc72b62d67f97608d366922d87d0a6c0e
GMonLep <gmonterolepio@gmail.com>
2026-04-07 15:23:44 -0400

    verificacion

--- src/ev_1/pipelines/data_validation/nodes.py
+++ src/ev_1/pipelines/data_validation/nodes.py
@@ -2,3 +2,44 @@
 This is a boilerplate pipeline 'data_validation'
 generated using Kedro 1.3.0
 """
+import pandas as pd
+
+
+def validate_dataset(dataset_integrado, consultas_limpio):
+
+    report = {}
+
+    #validación integridad de los datos
+
+    report["total_rows"] = dataset_integrado.shape[0]
+    report["total_columns"] = dataset_integrado.shape[1]
+
+    report["missing_values"] = dataset_integrado.isnull().sum().sum()
+
+    report["duplicate_rows"] = dataset_integrado.duplicated().sum()
+
+    #validación columnas
+
+    expected_columns = [
+        "paciente_id",
+        "consulta_id"
+    ]
+
+    report["missing_expected_columns"] = [
+        col for col in expected_columns if col not in dataset_integrado.columns
+    ]
+
+    #antes y dsps
+
+    report["rows_before_cleaning"] = consultas_limpio.shape[0]
+    report["rows_after_transformation"] = dataset_integrado.shape[0]
+
+    report["row_difference"] = (
+        consultas_limpio.shape[0] - dataset_integrado.shape[0]
+    )
+
+    #convertir a dataframe
+
+    validation_report = pd.DataFrame([report])
+
+    return validation_report
--- src/ev_1/pipelines/data_validation/pipeline.py
+++ src/ev_1/pipelines/data_validation/pipeline.py
@@ -4,7 +4,17 @@ generated using Kedro 1.3.0
 """
 
 from kedro.pipeline import Node, Pipeline  # noqa
-
+from .nodes import validate_dataset
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([])
+    return Pipeline([
+        Node(
+                func=validate_dataset,
+                inputs=[
+                    "dataset_integrado",
+                    "consultas_limpio"
+                ],
+                outputs="validation_report",
+                name="validate_integrated_dataset_node",
+            )
+    ])
